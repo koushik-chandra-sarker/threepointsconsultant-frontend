f415e1688056ef101f8698e70c7de83626e62b9f
koushik-stack <koushiksk.ks@gmail.com>
2022-02-19 19:26:57 +0600

    home page and responsive menu

--- src/layout/Footer/Footer.js
+++ src/layout/Footer/Footer.js
@@ -3,38 +3,43 @@ import {EMAIL, FACEBOOK, INSTAGRAM, LINKEDIN, PHONES1, PHONES2, TWITTER} from ".
 
 const Footer = () => {
     return (
-        <footer className={'flex justify-between'}>
+        <footer className={'flex justify-between md:flex-nowrap flex-wrap'}>
             {/*copyright */}
-           <div className={"text-gray-400 flex items-center"}><div><i className="far fa-copyright"/> Copyright {new Date().getFullYear()} 3 Point Consultant. All rights reserved</div></div>
-           <div className={'flex gap-4'}>
-               {/*social icons*/}
-               <div className={"flex justify-center items-end border-r-2"}>
-                   <a href={FACEBOOK} target={"_blank"} rel={"noopener noreferrer"}>
-                       <i className={"text-gray-400 hover:text-gray-500 mx-2 fab fa-facebook-square"}/>
-                   </a>
-                   <a href={LINKEDIN} target={"_blank"} rel={"noopener noreferrer"}>
-                       <i className={"text-gray-400 hover:text-gray-500 mx-2 fab fa-linkedin"}/>
-                   </a>
-                   <a href={INSTAGRAM} target={"_blank"} rel={"noopener noreferrer"}>
-                       <i className={"text-gray-400 hover:text-gray-500 mx-2 fab fa-instagram"}/>
-                   </a>
-                   <a href={TWITTER} target={"_blank"} rel={"noopener noreferrer"}>
-                       <i className={"text-gray-400 hover:text-gray-500 mx-2 fab fa-twitter"}/>
-                   </a>
-               </div>
-               {/*contact details*/}
-               <div className={"flex justify-center flex-col"}>
-                   <div className={"text-gray-400"}>
-                       <i className={"fas fa-phone-square-alt mr-2"}/>
-                       <a href={`tel:${PHONES1}`}>{PHONES1}</a>;
-                       <a href={`tel:${PHONES2}`}>{PHONES2}</a>
-                   </div>
-                   <div className={"text-gray-400"}>
-                       <i className={"fas fa-envelope mr-2"}/>
-                       <a href={`mailto:${EMAIL}`}>{EMAIL}</a>
-                   </div>
-               </div>
-           </div>
+            <div
+                className={"text-gray-400 flex items-center md:text-base text-xs text-center md:text-left"}>
+                <div><i className="far fa-copyright"/> Copyright {new Date().getFullYear()}
+                    3 Point Consultant. All rights reserved
+                </div>
+            </div>
+            <div className={'flex flex-col md:flex-row w-full md:w-auto text-center md:text-left gap-4 md:mt-0 mt-5  md:text-base text-xs '}>
+                {/*social icons*/}
+                <div className={"flex justify-center items-end border-r-2 "}>
+                    <a href={FACEBOOK} target={"_blank"} rel={"noopener noreferrer"}>
+                        <i className={"text-gray-400 hover:text-gray-500 mx-2 fab fa-facebook-square"}/>
+                    </a>
+                    <a href={LINKEDIN} target={"_blank"} rel={"noopener noreferrer"}>
+                        <i className={"text-gray-400 hover:text-gray-500 mx-2 fab fa-linkedin"}/>
+                    </a>
+                    <a href={INSTAGRAM} target={"_blank"} rel={"noopener noreferrer"}>
+                        <i className={"text-gray-400 hover:text-gray-500 mx-2 fab fa-instagram"}/>
+                    </a>
+                    <a href={TWITTER} target={"_blank"} rel={"noopener noreferrer"}>
+                        <i className={"text-gray-400 hover:text-gray-500 mx-2 fab fa-twitter"}/>
+                    </a>
+                </div>
+                {/*contact details*/}
+                <div className={"flex justify-center flex-col"}>
+                    <div className={"text-gray-400"}>
+                        <i className={"fas fa-phone-square-alt mr-2"}/>
+                        <a href={`tel:${PHONES1}`}>{PHONES1}</a>;
+                        <a href={`tel:${PHONES2}`}>{PHONES2}</a>
+                    </div>
+                    <div className={"text-gray-400"}>
+                        <i className={"fas fa-envelope mr-2"}/>
+                        <a href={`mailto:${EMAIL}`}>{EMAIL}</a>
+                    </div>
+                </div>
+            </div>
         </footer>
     );
 };
--- src/layout/Header/Header.js
+++ src/layout/Header/Header.js
@@ -1,38 +1,66 @@
 import React from 'react';
 import logo from "../../assets/images/logo .png"
-import {Link} from "react-router-dom";
+import {Link, NavLink} from "react-router-dom";
 import {SITE_URL} from "../../constants";
-
+import homeCss from "./Header.module.scss"
 const Header = () => {
+    const[activeHamburger, setActiveHamburger] = React.useState(false);
     return (
-
-        <nav
-            className="relative w-full flex items-center justify-between py-4 text-gray-500">
-            <div className=" w-full items-center justify-between">
-                <a className="w-32 flex items-center text-gray-900 hover:text-gray-900 focus:text-gray-900 mt-2 lg:mt-0 mr-1"
-                   href="#">
-                    <img src={logo} alt="logo" loading="lazy"/>
-                </a>
-            </div>
-            <div>
-                {/*share and search*/}
-                <div className={"flex gap-4 justify-end mb-1"}>
-                    <a rel={"noreferrer"} target={"_blank"} href={`https://www.facebook.com/sharer.php?u=${SITE_URL}`}>
-                        <i className="fas fa-share-alt text-gray-400"/>
+        <div>
+            <nav
+                className="relative w-full flex items-center justify-between py-4 text-gray-500">
+                <div className=" w-full items-center justify-between">
+                    <a className="w-32 flex items-center text-gray-900 hover:text-gray-900 focus:text-gray-900 mt-2 lg:mt-0 mr-1"
+                       href="#">
+                        <img src={logo} alt="logo" loading="lazy"/>
                     </a>
-                    <i className="fas fa-search text-gray-400 mt-1"/>
                 </div>
-                <ul class="w-full flex pl-0 list-style-none">
-                    <li class="px-2 whitespace-nowrap "><Link to={"/"}>Home</Link></li>
-                    <li class="px-2 whitespace-nowrap border-l-2"><Link to={"/about"}>About Us</Link></li>
-                    <li class="px-2 whitespace-nowrap border-l-2"><Link to={"/project"}>Projects</Link></li>
-                    <li class="px-2 whitespace-nowrap border-l-2"><Link to={"/news"}>News</Link></li>
-                    <li class="px-2 whitespace-nowrap border-l-2"><Link to={"/publication"}>Publications</Link></li>
-                    <li class="pl-2 whitespace-nowrap border-l-2"><Link to={"/contact"}>Contact</Link></li>
-                </ul>
+                <div>
+                    {/*share and search*/}
+                    <div className={"flex gap-4 justify-end mb-1"}>
+                        <a rel={"noreferrer"} target={"_blank"}
+                           href={`https://www.facebook.com/sharer.php?u=${SITE_URL}`}>
+                            <i className="fas fa-share-alt text-gray-400"/>
+                        </a>
+                        <i className="fas fa-search text-gray-400 mt-1"/>
+                    </div>
+                    <div className={"flex justify-end w-full"}>
+                        <div id={"Hamburger"}
+                             onClick={()=>setActiveHamburger(!activeHamburger)}
+                             className={`${homeCss.hamburger} ${activeHamburger? homeCss.hamburgerActive:""} md:hidden block`}>
+                            <span className={homeCss.line1}/>
+                            <span className={homeCss.line2}/>
+                            <span className={homeCss.line3}/>
+                        </div>
+                    </div>
+                    {/* Nav desktop view */}
+                    <ul className="w-full hidden md:flex pl-0 list-style-none ">
+                        <li className="px-2 whitespace-nowrap "><Link to={"/"}>Home</Link></li>
+                        <li className="px-2 whitespace-nowrap border-l-2"><Link to={"/about"}>About Us</Link></li>
+                        <li className="px-2 whitespace-nowrap border-l-2"><Link to={"/project"}>Projects</Link></li>
+                        <li className="px-2 whitespace-nowrap border-l-2"><Link to={"/news"}>News</Link></li>
+                        <li className="px-2 whitespace-nowrap border-l-2"><Link to={"/publication"}>Publications</Link>
+                        </li>
+                        <li className="pl-2 whitespace-nowrap border-l-2"><Link to={"/contact"}>Contact</Link></li>
+                    </ul>
 
+
+                </div>
+            </nav>
+            {/* Nav mobile view */}
+            <div className={!activeHamburger? homeCss.hamburgerActive:""}>
+                <ul className={`${homeCss.mobileMenu} "w-full md:hidden flex flex-col gap-2 border mb-5 p-5 list-style-none"`}>
+                    <li className="px-2 whitespace-nowrap "><NavLink to={"/"} className={"w-full block"}>Home</NavLink></li>
+                    <li className="px-2 whitespace-nowrap"><NavLink to={"/about"}>About Us</NavLink></li>
+                    <li className="px-2 whitespace-nowrap"><NavLink to={"/project"}>Projects</NavLink></li>
+                    <li className="px-2 whitespace-nowrap"><NavLink to={"/news"}>News</NavLink></li>
+                    <li className="px-2 whitespace-nowrap"><NavLink to={"/publication"}>Publications</NavLink></li>
+                    <li className="pl-2 whitespace-nowrap"><NavLink to={"/contact"}>Contact</NavLink></li>
+                </ul>
             </div>
-        </nav>
+
+        </div>
+
 
     );
 };
